42a6f62bcb1939d83d8e4e34cb156a2d0053d2fb
lizmery <l.vigil096@gmail.com>
2021-07-02 18:05:59 -0700

    Updated 'Reviews' section
    Fixed responsiveness of the review cards

--- javajam.js
+++ javajam.js
@@ -1,14 +1,47 @@
+/* Hamburger Menu */
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('change');
 });
 
+/* Review Section Slider */
+let slidePosition = 0;
+const slides = document.getElementsByClassName('review');
+const totalSlides = slides.length;
 
-
-/*
-const menuIcon = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
-
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('change');
+document.getElementById('nextBtn').addEventListener('click', function() {
+    moveToNextSlide();
 });
-*/
+
+document.getElementById('prevBtn').addEventListener('click', function() {
+    moveToPrevSlide();
+});
+
+function updateSlidePosition () {
+    for (let slide of slides) {
+        slide.classList.remove('visible');
+        slide.classList.add('hidden');
+    }
+
+    slides[slidePosition].classList.add('visible');
+    slides[slidePosition].classList.remove('hidden');
+}
+
+function moveToNextSlide () {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide () {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
